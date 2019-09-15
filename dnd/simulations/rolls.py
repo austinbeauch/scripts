2e3cc4f44e0e7c1be0bd02b3d39c96f4884f9f6c
Austin Beauchamp <austinbeauch@gmail.com>
2019-09-14 18:43:44 -0700

    Update AC to be >20

--- dnd/simulations/rolls.py
+++ dnd/simulations/rolls.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 100000
+n = 10000
 prof = 5
-armours = list(range(5,21))
+armours = list(range(5,24))
 
 def attack(dmg:int, ac:int, profficiency=5, advantage=False, heavy=False):
     hit_mod = (profficiency-5) if heavy else profficiency  # heavy attack has -5 to hit
@@ -38,26 +38,26 @@ two_heavy_wreckless = []
 for armour in armours:
     d = []
 
-    two_normal_att = attack(12, armour)
-    one_normal_att = attack(8, armour) + attack(6, armour, 0)
-    two_wreckless_att = attack(12, armour, advantage=True)
-    one_wreckless_att = attack(8, armour, advantage=True) + attack(6, armour, 0, advantage=True)
-    two_heavy_att = attack(12, armour, heavy=True)
-    two_heavy_wreckless_att =  attack(12, armour, heavy=True, advantage=True)
+    two_normal_att = attack(10, armour)
+    one_normal_att = attack(8, armour) + attack(6, armour)
+    two_wreckless_att = attack(10, armour, advantage=True)
+    one_wreckless_att = attack(8, armour, advantage=True) + attack(6, armour, advantage=True)
+    #two_heavy_att = attack(10, armour, heavy=True)
+    #two_heavy_wreckless_att =  attack(10, armour, heavy=True, advantage=True)
 
     two_normal.append(two_normal_att.mean())
     one_normal.append(one_normal_att.mean())
     two_wreckless.append(two_wreckless_att.mean())
     one_wreckless.append(one_wreckless_att.mean())
-    two_heavy.append(two_heavy_att.mean())
-    two_heavy_wreckless.append(two_heavy_wreckless_att.mean())
+    #two_heavy.append(two_heavy_att.mean())
+    #two_heavy_wreckless.append(two_heavy_wreckless_att.mean())
         
 plt.plot(armours, two_normal, 'r.', label="two_normal")
 plt.plot(armours, one_normal, 'b.', label="one_normal")
 plt.plot(armours, two_wreckless, 'g.', label="two_advantage")
 plt.plot(armours, one_wreckless, 'y.', label="one_advantage")
-plt.plot(armours, two_heavy, 'c.', label="two_heavy")
-plt.plot(armours, two_heavy_wreckless, 'k.', label="two_heavy_advantage")
+#plt.plot(armours, two_heavy, 'c.', label="two_heavy")
+#plt.plot(armours, two_heavy_wreckless, 'k.', label="two_heavy_advantage")
 
 plt.legend()
 plt.ylabel("Average damage")
